bec33194b2bcd32cbb412edd906f4fb6130c80f8
JNBarrachina <jnb.pasadobinario@gmail.com>
2025-02-06 13:54:42 +0100

    PasswordValidator: First Steps(2)

--- pv.js
+++ pv.js
@@ -1,20 +1,29 @@
 const inputPass = document.getElementById("inputPassword");
-inputPass.addEventListener("click", validatePassword);
+inputPass.addEventListener("key", validatePassword);
+let password = "";
 
 function validatePassword() {
-    password = document.getElementById("inputPassword").value;
+    password += document.getElementById("inputPassword").value;
+    console.log(password);
 
-    if (password.lenght < 8) {
-        document.getElementById("validatorText").innerText =
-            "La contraseña no es segura";
-        document.getElementById("validatorText").style.color = "red";
-    } else if (8 < password.lenght < 10) {
-        document.getElementById("validatorText").innerText =
-            "La contraseña no es demasiado segura";
-        document.getElementById("validatorText").style.color = "orange";
-    } else {
+    if (password.length > 10) {
         document.getElementById("validatorText").innerText =
             "¡Contraseña segura!";
         document.getElementById("validatorText").style.color = "green";
+        document.getElementById("validatorText").style.visibility = "visible";
+    }
+
+    if (password.length < 10 && password.length > 8) {
+        document.getElementById("validatorText").innerText =
+            "La contraseña no es demasiado segura";
+        document.getElementById("validatorText").style.color = "orange";
+        document.getElementById("validatorText").style.visibility = "visible";
+    }
+
+    if (password.length < 8) {
+        document.getElementById("validatorText").innerText =
+            "La contraseña es insegura.";
+        document.getElementById("validatorText").style.color = "red";
+        document.getElementById("validatorText").style.visibility = "visible";
     }
 }
