a3ef8b50c4e4201ce838afe7058417bdc721f9d2
bernat alcudia <bernatalcudia@gmail.com>
2025-02-07 11:06:38 +0100

    add input name and button login click function to log

--- pv.js
+++ pv.js
@@ -1,22 +1,37 @@
+const inputName = document.getElementById("inputName")
 const inputPass = document.getElementById("inputPassword");
 inputPass.addEventListener("input", validatePassword);
 
+const login = document.createElement("button");
+
+login.setAttribute("id", "login");
+login.setAttribute('disabled', "true");
+
+
+login.innerText = 'Login';
+
 function validatePassword() {
     let changeText = document.getElementById("validatorText");
-    let password = document.getElementById("inputPassword").value;
+    let password = document.getElementById("inputPassword").value.length;
+    let name = document.getElementById("inputName").value.length;
 
-    if (password.length > 10) {
+    if (password > 10 && name > 0) {
         changeText.innerText = "¡Contraseña segura!";
         changeText.setAttribute("class", "segura");
+        login.style.backgroundColor = "blue";
+        login.removeAttribute("disabled");
+
     }
 
-    if (password.length < 10 && password.length >= 8) {
+    if (password < 10 && password >= 8) {
         changeText.innerText = "La contraseña no es demasiado segura.";
         changeText.setAttribute("class", "medioSegura");
     }
 
-    if (password.length < 8) {
+    if (password < 8) {
         changeText.innerText = "La contraseña es insegura.";
         changeText.setAttribute("class", "noSegura");
     }
 }
+
+document.body.appendChild(login);
