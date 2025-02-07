b8220de1f7b0a5633af9fcaa022a82eac45dc9d7
JNBarrachina <jnb.pasadobinario@gmail.com>
2025-02-07 10:21:57 +0100

    Refactoring code

--- pv.js
+++ pv.js
@@ -2,26 +2,21 @@ const inputPass = document.getElementById("inputPassword");
 inputPass.addEventListener("input", validatePassword);
 
 function validatePassword() {
+    let changeText = document.getElementById("validatorText");
     let password = document.getElementById("inputPassword").value;
 
     if (password.length > 10) {
-        document.getElementById("validatorText").innerText =
-            "¡Contraseña segura!";
-        document.getElementById("validatorText").style.color = "green";
-        document.getElementById("validatorText").style.visibility = "visible";
+        changeText.innerText = "¡Contraseña segura!";
+        changeText.setAttribute("class", "segura");
     }
 
     if (password.length < 10 && password.length >= 8) {
-        document.getElementById("validatorText").innerText =
-            "La contraseña no es demasiado segura.";
-        document.getElementById("validatorText").style.color = "orange";
-        document.getElementById("validatorText").style.visibility = "visible";
+        changeText.innerText = "La contraseña no es demasiado segura.";
+        changeText.setAttribute("class", "medioSegura");
     }
 
     if (password.length < 8) {
-        document.getElementById("validatorText").innerText =
-            "La contraseña es insegura.";
-        document.getElementById("validatorText").style.color = "red";
-        document.getElementById("validatorText").style.visibility = "visible";
+        changeText.innerText = "La contraseña es insegura.";
+        changeText.setAttribute("class", "noSegura");
     }
 }
